b1183d2a46111b97d411e3334b3c94e1176af965
Matteo Corain <m96.corain@gmail.com>
2019-11-10 12:26:24 -0600

    Project #3: progress bar for WebView

--- proj03/Project3/project3a/src/main/java/cs478/project3a/PhoneWebsiteActivity.java
+++ proj03/Project3/project3a/src/main/java/cs478/project3a/PhoneWebsiteActivity.java
@@ -3,7 +3,9 @@ package cs478.project3a;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebChromeClient;
 import android.webkit.WebView;
+import android.widget.ProgressBar;
 
 public class PhoneWebsiteActivity extends AppCompatActivity {
 
@@ -12,15 +14,26 @@ public class PhoneWebsiteActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_phone_website);
 
+        // Retrieve the URL to load
         String url = getIntent().getStringExtra("phoneUrl");
         if (url == null) {
             finish();
-        } else {
-            WebView phoneWebView = findViewById(R.id.phoneWebView);
-            phoneWebView.loadUrl(getIntent().getStringExtra("phoneUrl"));
-            phoneWebView.getSettings().setLoadsImagesAutomatically(true);
-            phoneWebView.getSettings().setJavaScriptEnabled(true);
         }
+
+        // Set up the WebView
+        WebView phoneWebView = findViewById(R.id.phoneWebView);
+        phoneWebView.loadUrl(url);
+        phoneWebView.getSettings().setLoadsImagesAutomatically(true);
+        phoneWebView.getSettings().setJavaScriptEnabled(true);
+
+        // Set up the progress bar
+        ProgressBar pageLoadProgress = findViewById(R.id.pageLoadProgress);
+        phoneWebView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public void onProgressChanged(WebView view, int newProgress) {
+                pageLoadProgress.setProgress(newProgress);
+            }
+        });
     }
 
     @Override
